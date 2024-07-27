62bb65ef6a80cf53e8995c14553dabb5438566dd
prokent <pro.kent676@gmail.com>
2024-07-27 22:38:43 +0200

    Fix port configuration

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # Используем переменную окружения DATABASE_URL для подключения к базе данных
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/pro.kent/Documents/GitHub/AzEr/blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your_secret_key')
